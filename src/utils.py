308b87b3516197f14ebf941f5385279e6b666efa
govinda sah <sahgovinda401@gmail.com>
2024-10-04 23:00:30 +0530

    hyper tunning have completed

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
-
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -24,13 +23,22 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
     try:
       report={}
 
       for i in range(len(list(models))):
           model=list(models.values())[i]
-          model.fit(x_train, y_train.ravel()) # train model
+          para=params[list(models.keys())[i]]
+
+          gs = GridSearchCV(model,para,cv=3)
+          gs.fit(x_train,y_train.ravel())
+
+          model.set_params(**gs.best_params_)
+          model.fit(x_train,y_train.ravel())
+
+
+        #   model.fit(x_train, y_train.ravel()) # train model
 
           y_train_pred=model.predict(x_train) # predict train model
           y_test_pred=model.predict(x_test) # predict test model
